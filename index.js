2b7862795b89b0e8d124a4c60f13790922c89cda
Elias Martinez <jemart218@gmail.com>
2020-08-21 21:28:21 -0700

    second spell and semantic check

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 
 function handleStartButton() {
   // when user clicks start submit type button
-  $('body').on('click','#inception', function(event) {
+  $('body').on('click','#js-inception', function(event) {
+    STORE.startTime = event.timeStamp;
     renderQuestionPage();
   })
 }
 
 function renderStartPage() {
   // when link is opened this is the first 'page' users see
-  console.log(`START OVER AGAIN`)
   $('#js-score').html(``)
   $('main').html(`
     <section>
@@ -18,7 +18,7 @@ function renderStartPage() {
         <img src='${STORE.images[13].src}' alt='${STORE.images[13].alt}' />
         <figcaption>${STORE.images[13].cap}</figcaption>
       </figure>
-      <button type='button' id='inception'>START</button>
+      <button type='button' id='js-inception'>START</button>
     </section>
     <div class='multiple-choices js-multiple-choices'></div>
   `)
@@ -51,12 +51,17 @@ function generateStrings() {
       </div>
     </section>`
   } else {
+    const duration = (STORE.endTime-STORE.startTime)/1000;
     return `
     <section class='results-page'>
-      <h3>Results of Chemistry Quiz</h3>
-      <form id='js-end' class='results-page'>
-        <p id='score-sheet'><strong><i>Score: ${Math.floor(STORE.score*100/13)}%</i></strong></p>
-        <button type='submit' id='end'>Restart</button>
+      <h3>Grade Results of Chemistry Quiz</h3>
+      <p id='score-sheet'><i>Score:  ${Math.floor(STORE.score*100/13)}%</i></p>
+      <h3>Time Elapsed in Chemistry Quiz</h3>
+      <p><i>Duration of Test:   ${Math.floor(duration/60)}min(s)</i></p>
+      <h3>Number of Quiz Attempts</h3>
+      <p><i>Attempts made:   ${STORE.retakes+1}</i></p>
+      <form id='js-end'>
+        <button type='submit'>Restart</button>
       </form>
       <div class='multiple-choices js-multiple-choices'></div>
     </section>`
@@ -66,12 +71,12 @@ function generateStrings() {
 function renderQuestionPage() {
   // when user clicks next button, new question and results page
   if (STORE.currentQuestion <= 12) {
-    $('#js-score').html(`<span><i>Score: ${Math.floor(STORE.score*100/13)}%</i></span>`);
-    const questionString = generateStrings(STORE.currentQuestion);
+    $('#js-score').html(`<i>Score: ${Math.floor(STORE.score*100/13)}%</i>`);
+    const questionString = generateStrings();
     $('main').html(questionString);
   } else {
-    const questionString = generateStrings(STORE.currentQuestion);
-    $('main').html(questionString);
+    const questionString = generateStrings();
+    $('main').append(questionString);
     STORE.score = 0;
   }
 }
@@ -90,14 +95,9 @@ function handleAnswerSubmission() {
   })
   $('main').on('click','#js-new-question', function(event) {
     if (STORE.currentQuestion > 12) {
-      $('#js-score').html(`Quiz ComQuiz Complete!`);
-      $('main').html(`
-        <header class='container'>
-          <h2>
-            Results Page
-          </h2>
-        </header>
-      `);
+      STORE.endTime = event.timeStamp;
+      $('#js-score').html(`Quiz Complete!`);
+      $('main').html(`<h2>Results Page</h2>`);
       renderQuestionPage();
       STORE.currentQuestion = 0;
     } else {
@@ -114,8 +114,8 @@ function renderFeedbackPage(choice) {
     $('#js-score').html(`<span>Score: ${Math.floor(STORE.score*100/13)}%</span>`);
     $('.js-multiple-choices').append(`
       <section class='feedback correct'>
-        <header><h3>Correct!</h3></header>
-        <p><i>"${STORE.questions[STORE.currentQuestion].answer}"</i></p>
+        <h3>Correct!</h3>
+        <i>"${STORE.questions[STORE.currentQuestion].answer}"</i>
         <button type='button' id='js-new-question'>NEXT</button>
       </section>
     `)
@@ -124,9 +124,9 @@ function renderFeedbackPage(choice) {
     $('#js-next').addClass('hidden');
     $('.js-multiple-choices').append(`
       <section class='feedback incorrect'>
-        <header><h3>Incorrect</h3></header>
+        <h3>Incorrect</h3>
         <p>The correct answer was: </p> 
-        <p><i>"${STORE.questions[STORE.currentQuestion].answer}"</i></p>
+        <i>"${STORE.questions[STORE.currentQuestion].answer}"</i>
         <button type='button' id='js-new-question'>NEXT</button>
       </section>
     `)
@@ -138,6 +138,7 @@ function handleRestart() {
   $('body').on('submit','#js-end', event => {
     event.preventDefault();
     renderStartPage();
+    STORE.retakes++
   })
 }
 
